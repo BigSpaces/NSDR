aedac03738d5022a2227f67238fb8546c839b519
BigSpaces <info@ithesound.com>
2022-12-03 20:31:11 +0100

    Passing parameters!

--- lib/nsdr_web/controllers/step_controller.ex
+++ lib/nsdr_web/controllers/step_controller.ex
@@ -2,14 +2,16 @@ defmodule NsdrWeb.StepController do
   use NsdrWeb, :controller
 
   def step1(conn, _params) do
+    IO.puts("HELLO THERE")
     render(conn, "index.html")
   end
 
-  def step2(conn, _params) do
-    render(conn, "step2.html")
+  def step2(conn, incoming_params) do
+    render(conn, "step2.html", incoming_params: incoming_params)
   end
 
   def step3(conn, _params) do
+
     render(conn, "step3.html")
   end
 
