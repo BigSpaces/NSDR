ca876ef970d3f2c6ef0bf33f72a52d566f6ecf3f
BigSpaces <info@ithesound.com>
2022-12-13 14:54:03 +0100

    Updated styles and options

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :nsdr, NsdrWeb.Endpoint,
   secret_key_base: "ZbuGRvmS0vqKKFKlmzV6FwNrAGIwY4VZfo6crNrGFoa8sANHtsKE7qJ2LRsSJTwi",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
